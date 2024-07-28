5122b5906ed6a2d0dc443183f9a539136fe159bd
jblathiya1507 <163373314+jblathiya1507@users.noreply.github.com>
2024-07-28 18:06:42 +0530

    create tables 31

--- AI_ML/AI_ML/settings.py
+++ AI_ML/AI_ML/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'AI_ML.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'Interviews',  #database name
+       'USER': 'postgres',  #username
+       'PASSWORD': 'post',  #login password
+       'HOST': 'localhost',  #host name
+       'PORT': '5432',  #port number
+   }
 }
 
 
